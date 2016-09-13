6a881c8b3310205f309a3d26e4198e9a63f5f7d5
Ivan Shvedunov <ishvedunov@mirantis.com>
2016-09-14 01:59:43 +0300

    Support DIND & homedir install & go1.7.1
    This should have been split to 3 commits, but as of now it's too hard to
    do so.

--- install.sh
+++ install.sh
@@ -17,59 +17,102 @@ vm_type=libvirt
 USE_VIRTUALBOX="${USE_VIRTUALBOX:-}"
 export ANSIBLE_ROLES_PATH=$script_dir/provisioning/roles
 
-if [ $# -gt 0 ]; then
-    if [ "$1" = "-d" ]; then
-        ansible_via_docker=y
-        shift
-    fi
-fi
-
-if [ $# -eq 0 ]; then
-    usage
-fi
-
-cmd="$1"
-shift
-
-if [ "$cmd" = "remote" ]; then
-    if [ $# -eq 0 ]; then
-        echo "must specify target hostname" 1>& 2
-    fi
-    target_hostname="$1"
-    shift
-fi
-
-k8s_repo_url=
-if [ $# -gt 0 ]; then
-    k8s_repo_url="$1"
-fi
-
-if [[ "$OSTYPE" == darwin* && "$cmd" != "remote" ]]; then
-    export USE_VIRTUALBOX=1
-else
-    export VAGRANT_DEFAULT_PROVIDER=libvirt
-    provider_args="--provider=libvirt"
-fi
-
-if [ -n "$USE_VIRTUALBOX" ]; then
-    vm_type=virtualbox
-fi
+function usage {
+    echo "Usage:" 1>&2
+    echo "  ./install.sh local [K8S_REPO_URL]        - provision the local machine"
+    echo "  ./install.sh remote HOST [K8S_REPO_URL]  - provision the remote host"
+    echo "  ./install.sh home [K8S_REPO_URL]         - install in current user's home directory"
+    echo "                                             (WiP; currently bash-only)"
+    echo "  ./install.sh vagrant [K8S_REPO_URL]      - install inside a Vagrant VM"
+    echo "  ./install.sh host                        - prepare host machine for devbox VM"
+    exit 1
+}
 
 function install_roles {
     mkdir -p $ANSIBLE_ROLES_PATH
     ansible-galaxy install -r requirements.yml
 }
 
-function usage {
-    echo "usage:" 1>&2
-    echo "  $0 vagrant [K8S_REPO_URL]"
-    echo "  $0 host"
-    echo "  $0 local [K8S_REPO_URL]"
-    echo "  $0 remote HOST [K8S_REPO_URL]"
-    exit 1
+function update_profile {
+    if [ ! -f "$1" ]; then
+        return 1
+    fi
+    if ! grep -q '#added-by-k8s-devbox' "$1"; then
+        echo >>"$1"
+        echo "K8S_DEVBOX_HOMEDIR_INSTALL=true . '$devbox_dir'/k8s-devenv.sh #added-by-k8s-devbox" >> "$1"
+    fi
+}
+
+function install_go {
+    rm -rf "$devbox_dir/go"
+    cd "$devbox_dir"
+    if [ "$(uname)" == "Darwin" ]; then
+      go_tarball=go1.7.1.darwin-amd64.tar.gz
+      go_tarball_sha256="9fd80f19cc0097f35eaa3a52ee28795c5371bb6fac69d2acf70c22c02791f912"
+    else
+      go_tarball=go1.7.1.linux-amd64.tar.gz
+      go_tarball_sha256="43ad621c9b014cde8db17393dc108378d37bc853aa351a6c74bf6432c1bbd182"
+    fi
+    rm -f "$go_tarball"
+    wget https://storage.googleapis.com/golang/"$go_tarball"
+    if ! echo "$go_tarball_sha256  $go_tarball" | sha256sum -c -; then
+        echo "Go tarball checksum verification failed" 1>&2
+        exit 1
+    fi
+    tar -xzf "$go_tarball"
+    rm -f "$go_tarball"
+    export GOROOT="$devbox_dir/go"
+    export PATH="$devbox_dir/go/bin:$PATH"
+}
+
+function install_go_tools {
+    mkdir -p "$devbox_dir/go-tools"
+    export GOPATH="$devbox_dir/go-tools"
+    go get -u github.com/tools/godep
+    go get -u github.com/jteeuwen/go-bindata/go-bindata
+}
+
+function install_to_home_dir {
+    devbox_dir="$HOME"/.k8s-devbox
+    mkdir -p "$devbox_dir"
+    cp "$script_dir"/provisioning/files/k8s-devenv.sh "$devbox_dir"
+    cp "$script_dir"/provisioning/files/motd "$devbox_dir/help.txt"
+    install_go
+    install_go_tools
+    # based on from https://github.com/moovweb/gvm/blob/604e702e2a155b33c2f217f1f4931188344d4926/binscripts/gvm-installer#L96
+    if [ -n "${ZSH_NAME:-}" ]; then
+        echo "Sorry, zsh isn't supported yet" 1>&2
+        exit 1
+    elif [ "$(uname)" == "Linux" ]; then
+        update_profile "$HOME/.bashrc" || update_profile "$HOME/.bash_profile"
+    elif [ "$(uname)" == "Darwin" ]; then
+        update_profile "$HOME/.profile" || update_profile "$HOME/.bash_profile"
+    fi
+    if [ -n "$k8s_repo_url" -a ! -d $HOME/work/kubernetes/k8s.io/src/kubernetes ]; then
+        mkdir -p "$HOME/work/kubernetes/k8s.io/src"
+        git clone "$k8s_repo_url" "$HOME/work/kubernetes/k8s.io/src/kubernetes"
+    fi
+    echo 1>&2
+    echo "Please restart your shell to start using k8s-devbox." 1>&2
 }
 
 function install_using_vagrant {
+    if ! hash vagrant 2>/dev/null; then
+        echo "You need to install Vagrant" 1>&2
+        exit 1
+    fi
+
+    if [ -z "$USE_VIRTUALBOX" ] && vagrant plugin list | grep -q '^vagrant-libvirt'; then
+        export VAGRANT_DEFAULT_PROVIDER=libvirt
+        provider_args="--provider=libvirt"
+    else
+        export USE_VIRTUALBOX=1
+    fi
+
+    if [ -n "$USE_VIRTUALBOX" ]; then
+        vm_type=virtualbox
+    fi
+
     # https://kushaldas.in/posts/storage-volume-error-in-libvirt-with-vagrant.html
     # FIXME: happens too often for me for some reason
     virsh pool-refresh tmp >& /dev/null || true
@@ -100,12 +143,39 @@ function install_remotely {
     install_via_ansible -i "$target_hostname", --ssh-extra-args="-oForwardAgent=yes"
 }
 
-if ! hash ansible-playbook 2>&1; then
+if [ $# -gt 0 ]; then
+    if [ "$1" = "-d" ]; then
+        ansible_via_docker=y
+        shift
+    fi
+fi
+
+if [ $# -eq 0 ]; then
+    usage
+fi
+
+cmd="$1"
+shift
+
+if [ "$cmd" = "remote" ]; then
+    if [ $# -eq 0 ]; then
+        echo "must specify target hostname" 1>& 2
+    fi
+    target_hostname="$1"
+    shift
+fi
+
+k8s_repo_url=
+if [ $# -gt 0 ]; then
+    k8s_repo_url="$1"
+fi
+
+if ! hash ansible-playbook 2>/dev/null; then
     ansible_via_docker=y
 fi
 
 if [ -n "$ansible_via_docker" ]; then
-    echo "WiP: ansible invocation via docker doesn't work currently for vagrant due to ssh & file perm problems" 1>& 2
+    echo "WiP: ansible invocation via docker doesn't work currently due to ssh & file perm problems" 1>& 2
     exit 1
     # export PATH="$script_dir/ansible-via-docker:$PATH"
 fi
@@ -122,6 +192,9 @@ case "$cmd" in
         ;;
     remote)
         install_remotely
+        ;;
+    home)
+        install_to_home_dir
         ;;
     *)
         usage
--- provisioning/files/k8s-devenv.sh
+++ provisioning/files/k8s-devenv.sh
@@ -1,4 +1,9 @@
+K8S_DEVBOX_FULL_ENV="${K8S_DEVBOX_FULL_ENV:-}"
 if [ -f /vagrant_devbox ]; then
+    K8S_DEVBOX_FULL_ENV=true
+fi
+
+if [ -n "$K8S_DEVBOX_FULL_ENV" ]; then
     # In case of vagrant VM, provide a useful default prompt and go to k8s directory
     # The prompts gets overridden in default ~/.bashrc,
     # so reset it even if this script was already sourced
@@ -10,53 +15,170 @@ fi
 export KPATH=$HOME/work/kubernetes
 export GOPATH=$KPATH
 export KUBERNETES_SRC_DIR=$KPATH/src/k8s.io/kubernetes
-export KUBERNETES_PROVIDER=vagrant
-export PATH=$HOME/go-tools/bin:$KPATH/bin:$PATH
-
-if systemctl -q is-active virtualbox; then
-    export VAGRANT_DEFAULT_PROVIDER=virtualbox
-elif systemctl -q is-active libvirt-bin; then
-    export VAGRANT_DEFAULT_PROVIDER=libvirt
-# The following was causing ansible's scp to fail
-# with 'Received message too long'  
-#else
-#    echo "WARNING: no active virtualbox or libvirt-bin service detected"
+if [ -n "${K8S_DEVBOX_HOMEDIR_INSTALL:-}" ]; then
+    export GOROOT="$HOME/.k8s-devbox/go"
+    export PATH="$HOME/.k8s-devbox/go-tools/bin:$GOROOT/bin:$KPATH/bin:$KUBERNETES_SRC_DIR/_output/bin:$PATH"
+else
+    export PATH=$HOME/go-tools/bin:$KPATH/bin:$KUBERNETES_SRC_DIR/_output/bin:$PATH
 fi
+
+if hash systemctl 2>/dev/null; then
+    if systemctl -q is-active virtualbox; then
+        export VAGRANT_DEFAULT_PROVIDER=virtualbox
+    elif systemctl -q is-active libvirt-bin; then
+        export VAGRANT_DEFAULT_PROVIDER=libvirt
+    # The following was causing ansible's scp to fail
+    # with 'Received message too long'
+    #else
+    #    echo "WARNING: no active virtualbox or libvirt-bin service detected"
+    fi
+fi
+
+trace_highlight_start="$(echo -ne "\x1b[100m\x1b[97m"; echo -n "+")"
+trace_highlight_end="$(echo -ne "\x1b[49m\x1b[39m")"
+
+function quote_arg {
+    if [[ "$arg" =~ " " ]]; then
+        echo -n " '$arg'" 1>&2
+    else
+        echo -n " $arg" 1>&2
+    fi
+}
+
+function quote_var {
+    if [[ "$1" =~ ([^=]+)=(.*\ .*) ]]; then
+        echo -n " ${BASH_REMATCH[1]}='${BASH_REMATCH[2]}'"
+    else
+        echo -n " $1"
+    fi
+}
+
+function fake_trace {
+    echo -n "$trace_highlight_start"
+    for arg in "$@"; do
+        quote_arg "$arg"
+    done
+    echo "$trace_highlight_end"
+}
+
+function trace {
+    # have to use this tricky alternative to set -x becuause
+    # we want to highlight the trace lines
+    echo -n "$trace_highlight_start"
+
+    if [ "$1" = "export" ]; then
+        # don't execute 'export' in a subshell
+        echo -n " export"
+        shift
+        for arg in "$@"; do
+            quote_var "$arg"
+        done
+        echo "$trace_highlight_end"
+        export "$@"
+        return 0
+    fi
+
+    (
+        # Use subshell because var assignments are translated
+        # to 'export' because "$@" below can't parse them,
+        # but we don't want to
+        while [[ "$1" =~ "=" ]]; do
+            quote_var "$1"
+            export "$1"
+            shift
+        done
+        for arg in "$@"; do
+            quote_arg "$arg"
+        done
+        echo "$trace_highlight_end"
+        "$@"
+    )
+}
+
+function escape_test_name() {
+    sed 's/[]\$*.^|()[]/\\&/g; s/\s\+/\\s+/g' <<< "$1" | tr -d '\n'
+}
 
 function cdk {
     cd "$KUBERNETES_SRC_DIR"
 }
 
-if [ -f /vagrant_devbox -a -d "$KUBERNETES_SRC_DIR" ]; then
+if [ -n "$K8S_DEVBOX_FULL_ENV" -a -d "$KUBERNETES_SRC_DIR" ]; then
     cdk
 fi
 
 alias kubectl=$KUBERNETES_SRC_DIR/cluster/kubectl.sh
 
 function use-vagrant {
-    set -x
-    export KUBERNETES_PROVIDER=vagrant
-    kubectl config use-context vagrant
-    { set +x; } 2>/dev/null
+    trace export KUBERNETES_PROVIDER=vagrant
+    trace kubectl config use-context vagrant
 }
 
-function kube-up {
-    set -x
-    KUBERNETES_VAGRANT_USE_NFS=true \
+function dind-up {
+    cdk
+    if [ ! -d dind ]; then
+        # TODO: switch to https://github.com/sttts/kubernetes-dind-cluster
+        # after https://github.com/sttts/kubernetes-dind-cluster/pull/1 is merged
+        trace git clone https://github.com/ivan4th/kubernetes-dind-cluster.git dind
+    fi
+    if [ "$(uname)" == "Darwin" ]; then
+        if [ ! -f _output/dockerized/bin/linux/amd64/hyperkube ]; then
+            trace build/run.sh make WHAT=cmd/hyperkube
+        fi
+    elif [ ! -f _output/bin/hyperkube ]; then
+        trace make WHAT=cmd/hyperkube
+    fi
+    if [ ! -f _output/bin/kubectl ]; then
+        make WHAT=cmd/kubectl
+    fi
+    local -a args
+    while true; do
+        if [ "${1:-}" = "quick" ]; then
+            args=("${args[@]}" DOCKER_IN_DOCKER_SKIP_BUILD=true)
+            shift
+        elif [[ "${1:-}" =~ [0-9]+ ]]; then
+            args=("${args[@]}" NUM_NODES="$1")
+            shift
+        else
+            break
+        fi
+    done
+    trace "${args[@]}" dind/dind-up-cluster.sh
+    trace export KUBERNETES_PROVIDER=dind
+}
+
+function dind-down {
+    cdk
+    if [ ! -d dind ]; then
+        echo "DIND cluster isn't installed, try dind-up first" 1>& 2
+        return 1
+    fi
+    trace dind/dind-down-cluster.sh
+}
+
+function use-dind {
+    if [ ! -d dind ]; then
+        echo "DIND cluster isn't installed, try dind-up first" 1>& 2
+        return 1
+    fi
+    trace export KUBERNETES_PROVIDER=dind
+    trace kubectl config use-context dind
+}
+
+function vagrant-up {
+    trace make quick-release
+    trace KUBERNETES_VAGRANT_USE_NFS=true \
       KUBERNETES_NODE_MEMORY=1024 \
       NUM_NODES=2 \
       KUBERNETES_PROVIDER=vagrant \
       cluster/kube-up.sh
-    { set +x; } 2>/dev/null
     use-vagrant
 }
 
-function kube-down {
-    set -x
-    NUM_NODES=2 \
+function vagrant-down {
+    trace NUM_NODES=2 \
       KUBERNETES_PROVIDER=vagrant \
       cluster/kube-down.sh
-    { set +x; } 2>/dev/null
 }
 
 function get-ext-ip {
@@ -66,75 +188,138 @@ function get-ext-ip {
 function list_e2e {
     (
         cdk
-        # thanks to @asalkeld
-        grep -R "framework.KubeDescribe" test/e2e/* | cut -d"(" -f2 | cut -d"," -f1
+        if [ ! -f _output/bin/e2e.test ]; then
+            trace make WHAT=test/e2e/e2e.test
+        fi
+        if [ ! -f _output/bin/ginkgo ]; then
+            make WHAT=vendor/github.com/onsi/ginkgo/ginkgo
+        fi
+        extra_opts=
+        if [ $# -gt 0 ]; then
+            focus="$(escape_test_name "$1")"
+            extra_opts="--ginkgo.focus=${focus}"
+        fi
+        fake_trace ginkgo _output/bin/e2e.test -- --prefix=e2e --network=e2e --ginkgo.dryRun --ginkgo.noColor --ginkgo.noisyPendings=false $extra_opts '2>&1' '|' 'some_ugly_filter...'
+        ginkgo _output/bin/e2e.test -- --prefix=e2e --network=e2e --ginkgo.dryRun --ginkgo.noColor --ginkgo.noisyPendings=false $extra_opts 2>&1 |
+            egrep -v '^[•SP ]*$'|awk '/^Will run [0-9]* of [0-9]*/{flag=1;next}/^Ran [0-9]* of [0-9]*/{flag=0}flag'
     )
+}
+
+function set_e2e_opts {
+    extra_opts=""
+    extra_test_args=""
+    # work around test_args problems with spaces
+    if [ "$KUBERNETES_PROVIDER" = "local" ]; then
+        ext_ip="$(get-ext-ip)"
+        # thanks to @asalkeld
+        trace export KUBE_MASTER_IP="$ext_ip"
+        trace export KUBE_MASTER="$ext_ip"
+        extra_opts="--check_node_count=false"
+        extra_test_args=" --host=http://$KUBE_MASTER_IP:8080"
+    elif [ "$KUBERNETES_PROVIDER" = "dind" ]; then
+        trace export KUBE_MASTER_IP="localhost"
+        trace export KUBE_MASTER="localhost"
+        extra_test_args=" --host=https://$KUBE_MASTER_IP:6443"
+    fi
 }
 
 function e2e {
     (
         cdk
-        extra_opts=""
-        extra_test_args=""
-        # work around test_args problems with spaces
-        if [ "$KUBERNETES_PROVIDER" = "local" ]; then
-            # thanks to @asalkeld
-            set -x
-            export KUBE_MASTER_IP="$ext_ip"
-            export KUBE_MASTER="$ext_ip"
-            { set +x; } 2>/dev/null
-            extra_opts="--check_node_count=false --check_version_skew=false"
-            extra_test_args=" --host=http://$KUBE_MASTER_IP:8080"
-            ext_ip="$(get-ext-ip)"
+        if [ ! -f _output/bin/e2e.test ]; then
+            trace make WHAT=test/e2e/e2e.test
         fi
+        if [ ! -f _output/bin/ginkgo ]; then
+            make WHAT=vendor/github.com/onsi/ginkgo/ginkgo
+        fi
+        if [ ! -f _output/bin/kubectl ]; then
+            make WHAT=cmd/kubectl
+        fi
+        set_e2e_opts
+        status=0
         if [ $# -gt 0 ]; then
-            focus="${1// /\\s}"
-            set -x
-            go run hack/e2e.go -v --test --test_args="--ginkgo.focus=${focus}${extra_test_args}" $extra_opts
-            { set +x; } 2>/dev/null
+            focus="$(escape_test_name "$1")"
+            trace go run hack/e2e.go -v  -check_version_skew=false --test --test_args="--ginkgo.focus=${focus}${extra_test_args}" $extra_opts
         else
             # run 'upstream' set of tests
-            set -x
-            go run ./hack/e2e.go -v --test \
+            trace go run ./hack/e2e.go -v --test  -check_version_skew=false \
                --test_args="--ginkgo.skip=\[Slow\]|\[Serial\]|\[Disruptive\]|\[Flaky\]|\[Feature:.+\]$extra_test_args" $extra_opts
-            { set +x; } 2>/dev/null
         fi
     )
+}
+
+function conformance {
+    num_nodes="$(kubectl get nodes -o name|wc -l)"
+    set_e2e_opts
+    trace GINKGO_PARALLEL_NODES=$num_nodes \
+          GINKGO_PARALLEL=y \
+          go run hack/e2e.go --v --test -check_version_skew=false \
+          --test_args="--ginkgo.focus=\[zConformance\] --ginkgo.skip=\[Serial\]${extra_test_args}" $extra_opts
+    # [Serial] tests fail on DIND cluster as of now
+    # trace KUBERNETES_CONFORMANCE_TEST=y \
+    #       go run hack/e2e.go --v --test -check_version_skew=false \
+    #       --test_args="--ginkgo.focus=\[Serial\].*\[zConformance\]${extra_test_args}" $extra_opts
 }
 
 function local-up {
     ext_ip="$(get-ext-ip)"
-    set -x
-    export KUBERNETES_PROVIDER=local
-    { set +x; } 2>/dev/null
-    (
-      set -x
-      KUBE_ENABLE_CLUSTER_DNS=true \
-        KUBELET_HOST="$ext_ip" \
-        HOSTNAME_OVERRIDE=$KUBELET_HOST \
-        API_HOST=$KUBELET_HOST \
-        hack/local-up-cluster.sh
-    )
+    trace KUBE_ENABLE_CLUSTER_DNS=true \
+      KUBELET_HOST="$ext_ip" \
+      HOSTNAME_OVERRIDE="$ext_ip" \
+      API_HOST="$ext_ip" \
+      ALLOW_SECURITY_CONTEXT=true \
+      hack/local-up-cluster.sh
 }
 
 function use-local {
     ext_ip="$(get-ext-ip)"
-    set -x
-    export KUBERNETES_PROVIDER=local
-    kubectl config set-cluster local --server="http://$ext_ip:8080" --insecure-skip-tls-verify=true
-    kubectl config set-context local --cluster=local
-    kubectl config use-context local
-    { set +x; } 2>/dev/null
+    trace export KUBERNETES_PROVIDER=local
+    trace kubectl config set-cluster local --server="http://$ext_ip:8080" --insecure-skip-tls-verify=true
+    trace kubectl config set-context local --cluster=local
+    trace kubectl config use-context local
 }
 
 function update-kubelet {
     cdk
-    set -x
-    make
+    trace make
     for node in node-1 node-2; do
-        NUM_NODES=2 vagrant ssh $node -- sudo systemctl stop kubelet.service
-        NUM_NODES=2 vagrant ssh $node -- 'sudo tee /usr/local/bin/kubelet>/dev/null' <_output/bin/kubelet
-        NUM_NODES=2 vagrant ssh $node -- sudo systemctl start kubelet.service
+        trace NUM_NODES=2 vagrant ssh $node -- sudo systemctl stop kubelet.service
+        trace NUM_NODES=2 vagrant ssh $node -- 'sudo tee /usr/local/bin/kubelet>/dev/null' <_output/bin/kubelet
+        trace NUM_NODES=2 vagrant ssh $node -- sudo systemctl start kubelet.service
     done
-    { set +x; } 2>/dev/null
 }
+
+function testit {
+    cdk
+    if [ $# -eq 0 ]; then
+        trace make test
+    elif [ $# -eq 1 ]; then
+        trace make test WHAT="$1" KUBE_GOFLAGS="-v"
+    else
+        trace make test WHAT="$1" KUBE_GOFLAGS="-v" KUBE_TEST_ARGS="-run $2"
+    fi
+}
+
+function devhelp {
+    if [ -f /vagrant_devbox ]; then
+        cat /etc/motd
+    elif [ -f ~/.k8s-devbox/help.txt ]; then
+        cat ~/.k8s-devbox/help.txt
+    else
+        echo "Help file not found" 1>&2
+    fi
+}
+
+if [ -f ~/.kube/config ] && context_str="$(grep -o 'current-context:.*' ~/.kube/config)" && [[ "$context_str" =~ :\ *([^ ]+) ]]; then
+    context="${BASH_REMATCH[1]}"
+    case "$context" in
+        dind|local|vagrant)
+            if [ "${KUBERNETES_PROVIDER:-}" != "$context" ]; then
+                if [ -n "$K8S_DEVBOX_FULL_ENV" ]; then
+                    trace export KUBERNETES_PROVIDER="$context"
+                else
+                    export KUBERNETES_PROVIDER="$context"
+                fi
+            fi
+    esac
+fi
