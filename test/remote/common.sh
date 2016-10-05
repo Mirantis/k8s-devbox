62fdacb5c8fa442731005226140e0b9b3d5ff141
Ivan Shvedunov <ishvedunov@mirantis.com>
2016-10-06 02:33:39 +0300

    Use expect for tests

--- test/remote/common.sh
+++ test/remote/common.sh
@@ -13,11 +13,5 @@ function devbox-test-e2e-simple () {
     echo "${extra_cmds}" >/tmp/extra_cmds
     # "sudo su" is needed for "relogin" because in some tests
     # the user was just added to 'docker' group
-    sudo su - "${USER}" -c "bash -lis" <<EOF
-cdk
-dind-up 8
-list_e2e DNS | grep "should provide DNS for the cluster"
-e2e "existing RC"
-dind-down
-EOF
+    time sudo su - "${USER}" -c "expect -f ${devbox_dir}/test/remote/e2e-simple.exp"
 }
